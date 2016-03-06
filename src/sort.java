f6adf2b22014b26bf644e3d3186046858d5581de
Rikko <rickard.e.1992@gmail.com>
2016-03-06 23:02:13 +0100

    Sorting JML almost finished
    Sorting JML almost finished

--- src/sort.java
+++ src/sort.java
@@ -3,15 +3,19 @@ import java.util.Arrays;
 public class sort {
 	
 	
-	//@ requires arrayToSort != null;
-	//@ ensures (\forall int i; (i >= 0 && i < arrayToSort.length-1) ; (arrayToSort[i] < arrayToSort[i+1]));
+	/*@ requires arrayToSort != null;
+	  @	ensures (\forall int i; 
+	  @			0 < i && i < arrayToSort.length; 
+	  @			arrayToSort[i-1] <= arrayToSort[i]) &&
+	  @			arrayToSort.length == \old(arrayToSort.length);
+	  @*/
 	public void sortArray(int[] arrayToSort) {
 		int buffer = 0;
 		System.out.println(Arrays.toString(arrayToSort));
-		
-		for(int j=1;j<arrayToSort.length-1;j++) {
-			for(int i=0;i<arrayToSort.length-1;i++) { 
-				if(arrayToSort[i]>arrayToSort[i+1]) {
+				
+		for(int j = 0; j < arrayToSort.length-1; j++) {
+			for(int i = 0; i < arrayToSort.length-1; i++) { 
+				if(arrayToSort[i] > arrayToSort[i+1]) {
 					buffer = arrayToSort[i+1];
 					arrayToSort[i+1] = arrayToSort[i];
 					arrayToSort[i] = buffer;
@@ -20,6 +24,14 @@ public class sort {
 		}
 		System.out.println(Arrays.toString(arrayToSort));
 	}
+	
+	/*@
+	  @ requires array != null;
+	  @ ensures (\exists int i; 
+	  @				0 <= i && i <= array.length-1;
+	  @				array[i] == member ==> \result == i);
+	  @ 
+	  @*/
 
 	public int membQuery(int[] array,int member) {
 		
